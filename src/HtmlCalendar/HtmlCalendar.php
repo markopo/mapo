217bf1cfa3351156c1101e5917d886ebf266a156
marko <marko_poikkimaki@hotmail.com>
2014-11-20 11:20:02 +0100

    HtmlCalendar

--- src/HtmlCalendar/HtmlCalendar.php
+++ src/HtmlCalendar/HtmlCalendar.php
@@ -8,6 +8,8 @@
 
 class HtmlCalendar extends Calendar {
 
+    CONST DAYS = 7;
+
     public function __construct(){
         parent::__construct();
 
@@ -28,12 +30,24 @@ class HtmlCalendar extends Calendar {
         $calendar = parent::getCalendar($year, $month);
 
         $htmlCal = "<div class='calendar'></div>";
+        $index = 0;
         foreach($calendar as $cal){
-            $htmlCal .= "<div class='calendarday' data-year='$cal->Year' data-month='$cal->Month' data-day='$cal->Day' >";
+            if($index % HtmlCalendar::DAYS == 0){
+                $htmlCal .= "<div class='calendar-row'>";
+            }
+
+            $htmlCal .= "<div class='calendar-day' data-year='$cal->Year' data-month='$cal->Month' data-day='$cal->Day' >";
+            $htmlCal .= "<div class='calendar-day-container'>";
             $htmlCal .= "<span class='day'>$cal->Day</span>";
       //      $htmlCal .= "";
             $htmlCal .= "</div>";
+            $htmlCal .= "</div>";
 
+            if($index % HtmlCalendar::DAYS == HtmlCalendar::DAYS-1){
+                $htmlCal .= "</div>";
+            }
+
+            $index += 1;
         }
         $htmlCal .= "</div>";
 
