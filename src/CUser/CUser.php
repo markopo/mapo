97027deb830582abc3ab27e805d6ab003d27ec30
marko <marko_poikkimaki@hotmail.com>
2014-12-20 14:57:59 +0100

    Cuser & login

--- src/CUser/CUser.php
+++ src/CUser/CUser.php
@@ -6,52 +6,23 @@
  * Time: 13:35
  */
 
-class CUser {
+class CUser extends CDatabase {
 
     CONST ACRONYM_LOGGEDIN = "acronym_loggedin";
     CONST NAME_LOGGEDIN = "name_loggedin";
 
-    private $options;                   // Options used when creating the PDO object
-    private $db   = null;               // The PDO object
-    private $stmt = null;               // The latest statement used to execute a query
-
-    private $debug;
-
-
     public function __construct($options, $debug=false){
-        $default = array(
-            'dsn' => null,
-            'username' => null,
-            'password' => null,
-            'driver_options' => null,
-            'fetch_style' => PDO::FETCH_OBJ,
-        );
-        $this->options = array_merge($default, $options);
-
-        $this->debug = $debug;
-
-        try {
-            $this->db = new PDO($this->options['dsn'], $this->options['username'], $this->options['password'], $this->options['driver_options']);
-        }
-        catch(Exception $e) {
-            throw new PDOException('Could not connect to database, hiding connection details.'); // Hide connection details.
-        }
-
-        $this->db->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $this->options['fetch_style']);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
+        parent::__construct($options, $debug);
     }
 
     public function Login($acronym, $password){
         $sql = "SELECT acronym, name FROM `user` WHERE acronym = :acronym AND password = md5(concat(:password, salt))";
+        $params = array();
+        $params[":acronym"] = array($acronym);
+        $params[":password"] = array($password);
 
-        $this->stmt = $this->db->prepare($sql);
+        $res = parent::Fetch($sql, $params);
 
-        $this->stmt->bindParam(":acronym", $acronym);
-        $this->stmt->bindParam(":password", $password);
-
-        $this->stmt->execute();
-        $res = $this->stmt->fetch();
         if($res != null && $res->acronym == $acronym){
             $_SESSION[CUser::ACRONYM_LOGGEDIN] = $res->acronym;
             $_SESSION[CUser::NAME_LOGGEDIN] = $res->name;
---
+++ webroot/js/login.js
@@ -0,0 +1,11 @@
+/**
+ * Created by marko on 20/12/2014.
+ */
+
+(function() {
+    $("input#username").val("").focus();
+    $("input#password").val(""); 
+
+
+
+})();
